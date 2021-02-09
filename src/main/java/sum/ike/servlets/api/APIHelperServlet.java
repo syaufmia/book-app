6f95e60fb7806d7fcb533695fd106a98adbe6407
safi <safiye.uzun@msg.group>
2021-02-09 21:09:13 +0100

    extracting methods to APIHelper-Class

---
+++ src/main/java/sum/ike/servlets/api/APIHelperServlet.java
@@ -0,0 +1,33 @@
+package sum.ike.servlets.api;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class APIHelperServlet extends HttpServlet {
+
+
+    public String getBody (HttpServletRequest request) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        BufferedReader br = request.getReader();
+        String line;
+        while ((line = br.readLine()) != null) {
+            sb.append(line);
+        }
+        br.close();
+        return sb.toString();
+    }
+
+    public String[] getSubURI (HttpServletRequest req) {
+        return (req.getRequestURI().substring(1)).split("/");
+    }
+
+    public boolean compareSubURITo (HttpServletRequest req, int index, String check, String check2) {
+        String[] uri = getSubURI(req);
+        return uri[index].equalsIgnoreCase(check) || uri[index].equalsIgnoreCase(check2);
+    }
+    public boolean subURIisInt (HttpServletRequest req, int index) {
+        return getSubURI(req)[index].matches("\\d++");
+    }
+}
--- src/main/java/sum/ike/servlets/api/AuthorAPIServlet.java
+++ src/main/java/sum/ike/servlets/api/AuthorAPIServlet.java
@@ -13,19 +13,20 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 
-import java.io.BufferedReader;
+
 import java.io.IOException;
 
 
 @WebServlet
 public class AuthorAPIServlet extends HttpServlet {
 
-
     FileManager fm = new FileManager();
     AuthorDao aDao = new AuthorDao();
     BookDao bDao = new BookDao();
     Gson gson = new Gson();
     AuthorXDao aXDao = new AuthorXDao();
+
+    APIHelperServlet helper = new APIHelperServlet();
 
     @Override
     protected void doGet (HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -35,15 +36,15 @@ public class AuthorAPIServlet extends HttpServlet {
         resp.setCharacterEncoding("UTF-8");
         resp.setContentType("application/json");
 
-        String[] uri = getSubURI(req);
+        String[] uri = helper.getSubURI(req);
         switch (uri.length) {
             case 4:
                 resp.getWriter().println(gson.toJson(aXDao.convertAuthorList(aDao.getAll())));
                 resp.setStatus(HttpServletResponse.SC_OK);
                 break;
             case 6:
-                if (compareSubURITo(req, 4, "id", "author_id")
-                        && subURIisInt(req, 5)
+                if (helper.compareSubURITo(req, 4, "id", "author_id")
+                        && helper.subURIisInt(req, 5)
                         && (aDao.idExists(Integer.parseInt(uri[5])))) {
                     resp.getWriter().println(gson.toJson(aXDao.convertAuthor(aDao.getAuthorByID(Integer.parseInt(uri[5])))));
                     resp.setStatus(HttpServletResponse.SC_OK);
@@ -66,12 +67,12 @@ public class AuthorAPIServlet extends HttpServlet {
         aDao.importData(fm.readCSVFileAsObjects("AuthorList.csv"));
         resp.setCharacterEncoding("UTF-8");
         resp.setContentType("application/json");
-        String[] uri = getSubURI(req);
+        String[] uri = helper.getSubURI(req);
         String body;
 
 
         if ((uri.length == 4)
-                && ((body = getBody(req)) != null)
+                && ((body = helper.getBody(req)) != null)
                 && !body.isEmpty()) {
             JsonParser parser = new JsonParser();
             JsonObject json = parser.parse(body).getAsJsonObject();
@@ -103,14 +104,14 @@ public class AuthorAPIServlet extends HttpServlet {
         aDao.importData(fm.readCSVFileAsObjects("AuthorList.csv"));
         resp.setCharacterEncoding("UTF-8");
         resp.setContentType("application/json");
-        String[] uri = getSubURI(req);
+        String[] uri = helper.getSubURI(req);
         String body;
 
         if ((uri.length == 6)
-                && (compareSubURITo(req, 4, "id", "author_id")
-                && (subURIisInt(req, 5)
+                && (helper.compareSubURITo(req, 4, "id", "author_id")
+                && (helper.subURIisInt(req, 5)
                 && (aDao.idExists(Integer.parseInt(uri[5])))
-                && ((body = getBody(req)) != null)
+                && ((body = helper.getBody(req)) != null)
                 && !body.isEmpty()))) {
             JsonParser parser = new JsonParser();
             JsonObject json = parser.parse(body).getAsJsonObject();
@@ -147,13 +148,13 @@ public class AuthorAPIServlet extends HttpServlet {
         resp.setCharacterEncoding("UTF-8");
         resp.setContentType("application/json");
 
-        String[] uri = getSubURI(req);
+        String[] uri = helper.getSubURI(req);
         if (uri.length == 6) {
-            if (compareSubURITo(req, 4, "id", "author_id")
-                    && subURIisInt(req, 5)
+            if (helper.compareSubURITo(req, 4, "id", "author_id")
+                    && helper.subURIisInt(req, 5)
                     && (aDao.idExists(Integer.parseInt(uri[5])))) {
                 aDao.delete(Integer.parseInt(uri[5]));
-                resp.setStatus(HttpServletResponse.SC_OK);
+                resp.setStatus(HttpServletResponse.SC_NO_CONTENT);
                 fm.writeObjectFileCSV(aDao.exportData(), "AuthorList.csv",FileManager.AUTHOR_TABLE_HEADER_ROW);
                 fm.writeObjectFileCSV(bDao.exportData(),"BookList.csv", FileManager.BOOK_TABLE_HEADER_ROW);
             } else {
@@ -168,26 +169,4 @@ public class AuthorAPIServlet extends HttpServlet {
 
     }
 
-    public String getBody (HttpServletRequest request) throws IOException {
-       StringBuilder sb = new StringBuilder();
-        BufferedReader br = request.getReader();
-        String line;
-        while ((line = br.readLine()) != null) {
-            sb.append(line);
-        }
-        br.close();
-        return sb.toString();
-    }
-
-    public String[] getSubURI (HttpServletRequest req) {
-        return (req.getRequestURI().substring(1)).split("/");
-    }
-
-    public boolean compareSubURITo (HttpServletRequest req, int index, String check, String check2) {
-        String[] uri = getSubURI(req);
-        return uri[index].equalsIgnoreCase(check) || uri[index].equalsIgnoreCase(check2);
-    }
-    public boolean subURIisInt (HttpServletRequest req, int index) {
-        return getSubURI(req)[index].matches("\\d++");
-    }
 }
