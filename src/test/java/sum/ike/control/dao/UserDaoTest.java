f0aaee17c4639d16764a3cb7167ad12c92c60417
safi <safiye.uzun@msg.group>
2021-04-26 11:11:55 +0200

    added Junit 5 Dependency

---
+++ src/test/java/sum/ike/control/dao/UserDaoTest.java
@@ -0,0 +1,12 @@
+package sum.ike.control.dao;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserDaoTest {
+
+    @Test
+    void userLoginCorrect () {
+    }
+}
