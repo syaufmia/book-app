a80d701b8abf8a526fe4c66ddfc56314acb4af31
safi <safiye.uzun@msg.group>
2021-11-24 20:48:37 +0100

    disable UI-tests; updated DOC

--- src/test/java/ui/UiTest.java
+++ src/test/java/ui/UiTest.java
@@ -16,23 +16,20 @@ public class UiTest {
     @Test
     @Disabled
     public void testUi() throws InterruptedException {
-        String driverPath = "C:\\dev\\env\\chromedriver\\chromedriver.exe";
-        System.setProperty("webdriver.chrome.driver", driverPath);
-        WebDriver webDriver = new ChromeDriver();
-        webDriver.manage().window().maximize();
-        webDriver.navigate().to("http://localhost:8080/book_app_war/");
-
-        webDriver.findElement(By.id("u-name-field")).sendKeys("syaufmia");
-        webDriver.findElement(By.id("p-word-field")).sendKeys("1234");
-        webDriver.findElement(By.id("submit-login")).click();
-
-        for (WebElement w : webDriver.findElements(By.className("alert-table-row"))) {
-            System.out.println(w.getText());
-//            Pattern pattern = Pattern.compile(".*\\d{2}.\\d{1}.\\d{4}.*");
-            System.out.println(w.getText().matches(".*\\d{1,2}.\\d{1,2}.\\d{4}.*"));
-        }
-        webDriver.close();
-
-
+//        String driverPath = "C:\\dev\\env\\chromedriver\\chromedriver.exe";
+//        System.setProperty("webdriver.chrome.driver", driverPath);
+//        WebDriver webDriver = new ChromeDriver();
+//        webDriver.manage().window().maximize();
+//        webDriver.navigate().to("http://localhost:8080/book_app_war/");
+//
+//        webDriver.findElement(By.id("u-name-field")).sendKeys("syaufmia");
+//        webDriver.findElement(By.id("p-word-field")).sendKeys("1234");
+//        webDriver.findElement(By.id("submit-login")).click();
+//
+//        for (WebElement w : webDriver.findElements(By.className("alert-table-row"))) {
+//            System.out.println(w.getText());
+//            System.out.println(w.getText().matches(".*\\d{1,2}.\\d{1,2}.\\d{4}.*"));
+//        }
+//        webDriver.close();
     }
 }
